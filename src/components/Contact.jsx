6a082bb5771b9b2bd315c0c525cc6dc5c0592862
Minhal Shanjer <mshanjer@gmail.com>
2024-02-11 20:46:46 -0500

    Add contacts section.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='relative w-full min-h-[300px] mx-auto bg-slate-500'>
+      <div className='flex justify-center'>
+        <div className='text-align-center'>
+        <h1 className='text-3xl'>
+          Contact Me
+        </h1>
+        <p className='max-w-[600px]'>
+          Looking for a contractor or freelance developer? Looking to hire a SWE or PM for a full time position? Please reach out below!
+        </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
